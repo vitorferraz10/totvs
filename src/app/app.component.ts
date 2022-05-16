5ab8539b3358212eb6b9fb7e82cef5bb4a03d39b
Vitor Ferraz <vitorferrazmaciel@gmail.com>
2022-05-16 11:57:49 -0300

    project complete with problems of dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Todo } from "./Todo";
+/* import { HttpClient } from "@angular/common/http"; */
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app works!';
+/*   public http: HttpClient; */
+  todos: Todo[] = [];
+  newTodo: string;
+  valueEdit: string;
+
+ /*  requestJsonServer(name: string, id?: number) {
+    const url = "http://localhost/3000/todos";
+    const body = {
+      name,
+      id,
+    };
+   
+    return this.http.post(url, body);
+  } */
+
+  saveTodo() {
+    if (this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+      this.todos.push(todo);
+     /*  this.requestJsonServer(todo.name); */
+      this.newTodo = "";
+    } else {
+      alert("Please Enter Todo");
+    }
+  }
+
+  done(id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  remove(id: number) {
+    this.todos.splice(id, 1);
+  }
+
+  toEdit(id: number) {
+    this.valueEdit = prompt("Edit task");
+    this.todos[id].name = this.valueEdit;
+  }
 }
